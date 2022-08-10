5a1417f7b8abcc2d46a48996b23fee9a95a71eaa
Caio <caio.vsg@hotmail.com>
2022-08-10 17:48:20 -0300

    adicionando Drawer na tela de menu

---
+++ lib/screens/home/components/home_drawer.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../../../shared/constants/style_constants.dart';
+
+class HomeDrawer extends StatelessWidget {
+  const HomeDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: kBackgroundColor,
+      child: ListView(children: <Widget>[
+        ListTile(
+          leading: const Icon(Icons.access_alarm, color: kOnSurfaceColor),
+          textColor: kOnSurfaceColor,
+          title: const Text('Notificações'),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: const Icon(Icons.add_call, color: kOnSurfaceColor),
+          textColor: kOnSurfaceColor,
+          title: const Text('Contatos'),
+          onTap: () {},
+        ),
+      ]),
+    );
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:thunderapp/screens/home/components/home_drawer.dart';
+import 'package:thunderapp/shared/constants/app_number_constants.dart';
+import 'package:thunderapp/shared/constants/style_constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,10 +9,17 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
+      appBar: AppBar(),
+      drawer: const HomeDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+            horizontal: kDefaultPadding - 30, vertical: kDefaultPadding - 30),
+        child: Stack(children: <Widget>[
+          Container(
+            color: kPrimaryColor,
+          )
+        ]),
       ),
-      drawer: Drawer(),
     );
   }
 }
