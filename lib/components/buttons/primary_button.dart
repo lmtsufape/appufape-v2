0991e99225e382f356927afef076377c62ea65b8
JoseDanielF <danielduartefilho.df@gmail.com>
2022-12-20 17:07:50 -0300

    Botões adicionados

--- lib/components/buttons/primary_button.dart
+++ lib/components/buttons/primary_button.dart
@@ -1,23 +1,28 @@
-import 'package:flutter/material.dart';
 import 'package:appufape/shared/constants/style_constants.dart';
+import 'package:flutter/material.dart';
 
 class PrimaryButton extends StatelessWidget {
-  const PrimaryButton({Key? key, required this.text, required this.onPressed})
-      : super(key: key);
+  const PrimaryButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    required this.color,
+  }) : super(key: key);
   final String text;
   final Function onPressed;
+  final Color color;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.6,
-      height: MediaQuery.of(context).size.height * 0.055,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.06,
       child: ElevatedButton(
-        style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(kPrimaryColor),
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25)))),
+        style: ElevatedButton.styleFrom(backgroundColor: color),
         onPressed: () => onPressed(),
-        child: Text(text, style: kcaption2),
+        child: Text(
+          text,
+          style: kTitle2,
+        ),
       ),
     );
   }
