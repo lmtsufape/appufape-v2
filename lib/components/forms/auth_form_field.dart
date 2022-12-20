5518b4d60ab7735017cf6471526ed5a805ee3ac0
JoseDanielF <danielduartefilho.df@gmail.com>
2022-12-20 17:08:00 -0300

    Formulários adicionados

---
+++ lib/components/forms/auth_form_field.dart
@@ -0,0 +1,57 @@
+import 'package:appufape/shared/constants/style_constants.dart';
+import 'package:flutter/material.dart';
+
+class AuthFormField extends StatefulWidget {
+  final String label;
+  final bool isPassword;
+  final TextInputType inputType;
+  final Function(String) onChanged;
+
+  const AuthFormField({
+    Key? key,
+    required this.label,
+    required this.isPassword,
+    required this.inputType,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  State<AuthFormField> createState() => _AuthFormFieldState();
+}
+
+class _AuthFormFieldState extends State<AuthFormField> {
+  bool showPassword = true;
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      height: size.height * 0.06,
+      child: TextFormField(
+        onChanged: widget.onChanged,
+        style: const TextStyle(color: kSecondaryColor),
+        obscureText: widget.isPassword ? showPassword : false,
+        keyboardType: widget.inputType,
+        decoration: InputDecoration(
+          suffixIcon: widget.isPassword
+              ? InkWell(
+                  onTap: () {
+                    setState(() {
+                      showPassword = !showPassword;
+                    });
+                  },
+                  child: Icon(
+                      showPassword ? Icons.visibility : Icons.visibility_off,
+                      color: kDetailColor))
+              : null,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: kDetailColor,
+              width: 1,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/components/forms/auth_form_field2.dart
@@ -0,0 +1,58 @@
+import 'package:appufape/shared/constants/style_constants.dart';
+import 'package:flutter/material.dart';
+
+class AuthFormField2 extends StatefulWidget {
+  final String label;
+  final bool isPassword;
+  final TextInputType inputType;
+  final Function(String) onChanged;
+
+  const AuthFormField2({
+    Key? key,
+    required this.label,
+    required this.isPassword,
+    required this.inputType,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  State<AuthFormField2> createState() => _AuthFormField2State();
+}
+
+class _AuthFormField2State extends State<AuthFormField2> {
+  bool showPassword = true;
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      height: size.height * 0.06,
+      width: size.width * 0.20,
+      child: TextFormField(
+        onChanged: widget.onChanged,
+        style: const TextStyle(color: kSecondaryColor),
+        obscureText: widget.isPassword ? showPassword : false,
+        keyboardType: widget.inputType,
+        decoration: InputDecoration(
+          suffixIcon: widget.isPassword
+              ? InkWell(
+                  onTap: () {
+                    setState(() {
+                      showPassword = !showPassword;
+                    });
+                  },
+                  child: Icon(
+                      showPassword ? Icons.visibility : Icons.visibility_off,
+                      color: kDetailColor))
+              : null,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              color: kDetailColor,
+              width: 1,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
