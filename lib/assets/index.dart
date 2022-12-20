9db8915c108c7517242fa4d9b37246f545f3e176
JoseDanielF <danielduartefilho.df@gmail.com>
2022-12-20 17:06:53 -0300

    Assets componentizados

--- lib/assets/index.dart
+++ lib/assets/index.dart
@@ -1,4 +1,9 @@
 class Assets {
-
-  static const String logo = 'assets/images/logo.png';
+  static const String logo = 'lib/assets/images/logo_lmts.png';
+  static const String calendar = 'lib/assets/images/calendar_month.png';
+  static const String mail = 'lib/assets/images/mail.png';
+  static const String pinDrop = 'lib/assets/images/pin_drop.png';
+  static const String qrCode = 'lib/assets/images/qr_code_2.png';
+  static const String school = 'lib/assets/images/school.png';
+  static const String textsnippet = 'lib/assets/images/text_snippet.png';
 }
