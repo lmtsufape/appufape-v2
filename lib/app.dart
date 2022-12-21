d6e5d03bed9fa2126cd3d51b56b36e8e7457ad31
JoseDanielF <danielduartefilho.df@gmail.com>
2022-12-21 20:29:28 -0300

    App atualizado

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,9 @@
+import 'package:appufape/screens/pages/calendario/calendario_page.dart';
+import 'package:appufape/screens/pages/contatos/contatos_page.dart';
+import 'package:appufape/screens/pages/cursos/cursos_page.dart';
+import 'package:appufape/screens/pages/editais/editais_page.dart';
+import 'package:appufape/screens/pages/mapa/mapa_page.dart';
+import 'package:appufape/screens/pages/qrcode/qrcode_page.dart';
 import 'package:appufape/screens/start/start_screen.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
@@ -39,7 +45,14 @@ class App extends StatelessWidget {
         Screens.home: (BuildContext context) => const HomeScreen(),
         Screens.signin: (BuildContext context) => const SignInScreen(),
         Screens.carousel: (BuildContext context) => const CarouselScreen(),
-        Screens.start: (BuildContext context) => const StartScreen()
+        Screens.start: (BuildContext context) => const StartScreen(),
+        Screens.calendario: (BuildContext context) => const CalendarioPage(),
+        Screens.contatos: (BuildContext context) => const ContatosPage(),
+        Screens.cursos: (BuildContext context) => const CursosPage(),
+        Screens.editais: (BuildContext context) => const EditaisPage(),
+        Screens.mapa: (BuildContext context) => const MapaPage(),
+        Screens.qrcode: (BuildContext context) => const QrCodePage(),
+        
       },
     );
   }
