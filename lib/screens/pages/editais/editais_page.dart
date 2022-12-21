acedb4b0f0418409553ff5aa625bea30ef04f10d
JoseDanielF <danielduartefilho.df@gmail.com>
2022-12-21 20:28:53 -0300

    Editais Page criada

---
+++ lib/screens/pages/editais/editais_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditaisPage extends StatefulWidget {
+  const EditaisPage({Key? key}) : super(key: key);
+
+  @override
+  State<EditaisPage> createState() => _EditaisPageState();
+}
+
+class _EditaisPageState extends State<EditaisPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
