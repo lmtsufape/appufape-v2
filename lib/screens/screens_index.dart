3ee52aee6af3e4c1f7c44b7f99bcec62e2c5ab96
JoseDanielF <danielduartefilho.df@gmail.com>
2022-12-21 20:29:22 -0300

    Novas telas componentizadas

--- lib/screens/screens_index.dart
+++ lib/screens/screens_index.dart
@@ -1,8 +1,13 @@
 class Screens {
-
   static const String home = '/home';
   static const String splash = '/splash';
   static const String signin = '/sign_in';
   static const String carousel = '/carousel';
   static const String start = '/start';
+  static const String calendario = 'pages/calendario/calendario_page.dart';
+  static const String contatos = 'pages/calendario/contatos_page.dart';
+  static const String cursos = 'pages/calendario/cursos_page.dart';
+  static const String editais = 'pages/calendario/editais_page.dart';
+  static const String mapa = 'pages/calendario/mapa_page.dart';
+  static const String qrcode = 'pages/calendario/qrcode_page.dart';
 }
