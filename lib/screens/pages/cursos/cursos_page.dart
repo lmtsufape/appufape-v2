c0dae3ea3951cc584a56bb1cae69c6eec01f8343
JoseDanielF <danielduartefilho.df@gmail.com>
2022-12-21 20:28:46 -0300

    Cursos Page criada

---
+++ lib/screens/pages/cursos/cursos_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CursosPage extends StatefulWidget {
+  const CursosPage({Key? key}) : super(key: key);
+
+  @override
+  State<CursosPage> createState() => _CursosPageState();
+}
+
+class _CursosPageState extends State<CursosPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
