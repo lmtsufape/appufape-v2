66146eb1875fcf05b27c60c9504e5780f6f300be
JoseDanielF <danielduartefilho.df@gmail.com>
2022-12-21 20:29:10 -0300

    Qr Code Page criada

---
+++ lib/screens/pages/qrcode/qrcode_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QrCodePage extends StatefulWidget {
+  const QrCodePage({Key? key}) : super(key: key);
+
+  @override
+  State<QrCodePage> createState() => _QrCodePageState();
+}
+
+class _QrCodePageState extends State<QrCodePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
