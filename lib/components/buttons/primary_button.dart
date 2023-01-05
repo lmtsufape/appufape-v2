dce16b36e3ce4b3529ff9423c43c27e2986b1da8
JoseDanielF <danielduartefilho.df@gmail.com>
2023-01-05 17:34:13 -0300

    Correção visual no botão primário

--- lib/components/buttons/primary_button.dart
+++ lib/components/buttons/primary_button.dart
@@ -14,14 +14,14 @@ class PrimaryButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: MediaQuery.of(context).size.width,
+      width: MediaQuery.of(context).size.width * 0.6,
       height: MediaQuery.of(context).size.height * 0.06,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(backgroundColor: color),
         onPressed: () => onPressed(),
         child: Text(
           text,
-          style: kTitle2,
+          style: kTitle3,
         ),
       ),
     );
