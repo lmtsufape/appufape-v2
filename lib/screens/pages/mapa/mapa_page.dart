e37a02149476942c66dab812dd9c4cbe9a82cc37
JoseDanielF <danielduartefilho.df@gmail.com>
2023-01-06 15:47:13 -0300

    Barras de navegaçao adicionadas

--- lib/screens/pages/mapa/mapa_page.dart
+++ lib/screens/pages/mapa/mapa_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:appufape/assets/index.dart';
-import 'package:appufape/components/utils/horizontal_spacer_box.dart';
 import 'package:appufape/components/utils/vertical_spacer_box.dart';
 import 'package:appufape/screens/home/home_screen_controller.dart';
-import 'package:appufape/screens/screens_index.dart';
 import 'package:appufape/shared/constants/app_enums.dart';
 import 'package:appufape/shared/constants/style_constants.dart';
 import 'package:provider/provider.dart';
@@ -23,6 +21,11 @@ class _MapaPageState extends State<MapaPage> {
       create: (_) => HomeScreenController(),
       builder: (context, child) => Consumer<HomeScreenController>(
         builder: ((context, controller, child) => Scaffold(
+              appBar: AppBar(
+                title: const Text('Mapa', style: kTitle2),
+                toolbarOpacity: 0,
+                backgroundColor:kOnSurfaceColor ,
+              ),
               body: Container(
                 color: kOnSurfaceColor,
                 width: size.width,
@@ -30,31 +33,6 @@ class _MapaPageState extends State<MapaPage> {
                 child: Column(
                     // ignore: prefer_const_literals_to_create_immutables
                     children: [
-                      const VerticalSpacerBox(size: SpacerSize.large),
-                      const Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Text('MAPA', style: kTitle),
-                      ),
-                      const VerticalSpacerBox(size: SpacerSize.large),
-                      const Padding(
-                        padding: EdgeInsets.all(5.0),
-                        child: TextField(
-                            keyboardType: TextInputType.text,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(30))),
-                              hintText: 'Faça sua busca',
-                              contentPadding: EdgeInsets.symmetric(
-                                  vertical: 15, horizontal: 15),
-                              isDense: true,
-                              prefixIcon: Icon(
-                                Icons.search,
-                                color: kText,
-                                size: 25,
-                              ),
-                            )),
-                      ),
                       const VerticalSpacerBox(size: SpacerSize.large),
                       InkWell(
                         child: Container(
@@ -82,7 +60,7 @@ class _MapaPageState extends State<MapaPage> {
                                     Center(
                                         child: Image.asset(
                                       Assets.mapa,
-                                      scale: 0.78,
+                                      scale: 0.8,
                                     )),
                                   ],
                                 ),
