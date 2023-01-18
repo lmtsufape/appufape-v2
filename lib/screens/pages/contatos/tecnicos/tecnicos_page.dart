ece88370445388cd7df56aa0065a5362e70fa488
JoseDanielF <danielduartefilho.df@gmail.com>
2023-01-18 19:01:51 -0300

    Tecnicos page criado

---
+++ lib/screens/pages/contatos/tecnicos/tecnicos_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TecnicosPage extends StatelessWidget {
+  const TecnicosPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
