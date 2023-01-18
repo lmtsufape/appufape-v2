70d6caab6158a8c5d7479fcfe31970926c5d20eb
JoseDanielF <danielduartefilho.df@gmail.com>
2023-01-18 19:01:33 -0300

    Docentes page criada

---
+++ lib/screens/pages/contatos/docentes/docentes_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DocentesPage extends StatelessWidget {
+  const DocentesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
