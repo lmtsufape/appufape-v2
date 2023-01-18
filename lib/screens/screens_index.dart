5a337d29b6640e9066a29085c10c718214746619
JoseDanielF <danielduartefilho.df@gmail.com>
2023-01-18 19:02:51 -0300

    Index das telas atualizadas

--- lib/screens/screens_index.dart
+++ lib/screens/screens_index.dart
@@ -5,12 +5,21 @@ class Screens {
   static const String carousel = '/carousel';
   static const String start = '/start';
   static const String calendario = 'pages/calendario/calendario_page.dart';
-  static const String contatos = 'pages/calendario/contatos_page.dart';
-  static const String cursos = 'pages/calendario/cursos_page.dart';
-  static const String editais = 'pages/calendario/editais_screen.dart';
-  static const String editalPage = 'pages/calendario/edital_page.dart';
-  static const String mapa = 'pages/calendario/mapa_page.dart';
-  static const String qrcode = 'pages/calendario/qrcode_page.dart';
-  static const String menu = 'pages/menu/menu.dart';
+  static const String contatos = 'pages/contatos/contatos_page.dart';
+  static const String docentes = 'pages/contatos/docentes/docentes_page.dart';
+  static const String estrutura =
+      'pages/contatos/estrutura/estrutura_page.dart';
+  static const String tecnicos = 'pages/contatos/tecnicos/tecnicos_page.dart';
+  static const String cursos = 'pages/cursos/cursos_page.dart';
+  static const String graduacao = 'pages/cursos/graducao/graducao_page.dart';
+  static const String mestrado = 'pages/cursos/mestrado/mestrado_page.dart';
+  static const String doutorado = 'pages/cursos/doutorado/doutorado_page.dart';
+  static const String especializacao =
+      'pages/cursos/especializacao/especializacao_page.dart';
+  static const String editais = 'pages/editais/editais_screen.dart';
+  static const String editalPage = 'pages/editais/edital_page.dart';
+  static const String mapa = 'pages/mapa/mapa_page.dart';
+  static const String qrcode = 'pages/qrcode/qrcode_page.dart';
+  static const String menu = 'home/menu/menu.dart';
   static const String noticias = 'pages/noticias/noticias_page.dart';
 }
