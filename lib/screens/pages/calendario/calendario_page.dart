8796bbc1473111de31c47b767ea5405b1f2f143a
JoseDanielF <danielduartefilho.df@gmail.com>
2023-01-25 20:13:18 -0300

    Adicionada bottomNavigator na tela de calendário

--- lib/screens/pages/calendario/calendario_page.dart
+++ lib/screens/pages/calendario/calendario_page.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:thunderapp/shared/components/BottomNavigation.dart';
+import 'package:thunderapp/shared/constants/style_constants.dart';
 
-class CalendarioPage extends StatefulWidget {
+class CalendarioPage extends StatelessWidget {
   const CalendarioPage({Key? key}) : super(key: key);
 
   @override
-  State<CalendarioPage> createState() => _CalendarioPageState();
-}
-
-class _CalendarioPageState extends State<CalendarioPage> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+     int selectedIndex = 0;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Calendário',
+          style: kTitle2,
+        ),
+        centerTitle: true,
+        toolbarOpacity: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+        backgroundColor: kOnSurfaceColor,
+      ),
+      backgroundColor: kOnSurfaceColor,
+      bottomNavigationBar: BottomNavigation(
+                    selectedIndex: selectedIndex,
+                  ),
+      body: const Center(
+        child: Text('Calendário'),
+      ),
+    );
   }
 }
