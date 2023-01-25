951cac5f08f8c9352185b9e3c84e64060658fb71
JoseDanielF <danielduartefilho.df@gmail.com>
2023-01-25 20:13:31 -0300

    Adicionada bottomNavigator na tela de docentes

--- lib/screens/pages/contatos/docentes/docentes_page.dart
+++ lib/screens/pages/contatos/docentes/docentes_page.dart
@@ -1,11 +1,41 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:thunderapp/shared/components/BottomNavigation.dart';
+import 'package:thunderapp/shared/constants/style_constants.dart';
 
 class DocentesPage extends StatelessWidget {
   const DocentesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    int selectedIndex = 0;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Docentes',
+          style: kTitle2,
+        ),
+        centerTitle: true,
+        toolbarOpacity: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+        backgroundColor: kOnSurfaceColor,
+      ),
+      bottomNavigationBar: BottomNavigation(
+                    selectedIndex: selectedIndex,
+                  ),
+      backgroundColor: kOnSurfaceColor,
+      body: const Center(
+        child: Text('Docentes'),
+      ),
+    );
   }
 }
