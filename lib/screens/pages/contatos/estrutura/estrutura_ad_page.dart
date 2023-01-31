69a6935de3ef734e462a3fe56454e7e670a63bb2
JoseDanielF <danielduartefilho.df@gmail.com>
2023-01-30 22:48:08 -0300

    Erro das rotas corrigido na tela de estrutura administrativa

--- lib/screens/pages/contatos/estrutura/estrutura_ad_page.dart
+++ lib/screens/pages/contatos/estrutura/estrutura_ad_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:thunderapp/shared/components/BottomNavigation.dart';
 
+import '../../../../shared/constants/app_number_constants.dart';
 import '../../../../shared/constants/style_constants.dart';
 
 class EstruturaAdPage extends StatelessWidget {
@@ -13,29 +14,25 @@ class EstruturaAdPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           'Estrutura Administrativa',
-          style: kTitle2,
+          style: kTitle22,
         ),
         centerTitle: true,
         toolbarOpacity: 0,
-        actions: [
-          IconButton(
-            icon: const Icon(
-              Icons.arrow_back,
-              color: Colors.black,
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ],
-        backgroundColor: kOnSurfaceColor,
+        backgroundColor: kBack1,
       ),
       backgroundColor: kOnSurfaceColor,
-       bottomNavigationBar: BottomNavigation(
-                    selectedIndex: selectedIndex,
-                  ),
-      body: const Center(
-        child: Text('Estrutura Administrativa'),
+      bottomNavigationBar: BottomNavigation(
+        selectedIndex: selectedIndex,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(kDefaultPadding),
+        child: Align(
+            alignment: Alignment.topCenter,
+            child: Text(
+              'Lista de nomes e e-mails da estrutura administrativos da UFAPE',
+              textAlign: TextAlign.center,
+              style: kTitle55,
+            )),
       ),
     );
   }
